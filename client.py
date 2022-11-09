3501a5732d252b990b6c58776b20df90fed14aad
l-velazquez <luisfernandojavier.velazquez@upr.edu>
2022-11-09 16:49:21 -0400

    Changes to the client - added connection to server

--- client.py
+++ client.py
@@ -1,20 +1,36 @@
 from struct import *
+from socket import *
 from time import *
 
+#Adress to connect to the server
 ADDRESS = "136.145.181.51"
 PORT = 4205
+serverAddrPort = (ADDRESS,PORT)
 bufferSize = 1024
 
+s = socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-print("Please choose one operation:\n\t1. Sum\n\t2.Substraction\n\t3.Muliplication\n\t4.Division\n\t5.Factorial\n\tSummatory")
-inp = input("\nOperation >>> ")
+#Print to terminal for the client
+print("\n\nPlease choose one operation:\n\t1.Sum\n\t2.Substraction\n\t3.Muliplication\n\t4.Division\n\t5.Factorial\n\t6.Summatory")
+inp = int(input("\nOperation >>> "))
 
-print()
-
-if(inp == 1 or inp == 2 or inp == 3 or inp ==4):
+if(inp == (1 or 2 or 3 or 4)):
     print("Please add two inputs:")
-    inp1 = input("Input 1 >>>")
-    inp2 = input("Input 2 >>>")
+    inp1 = int(input("Input 1 >>>"))
+    inp2 = int(input("Input 2 >>>"))
+    inp2send = (inp,inp1,inp2)
 else:
     print("Input number")
-    inp1 = input("Input >>>")
+    inp1 = int(input("Input >>>"))
+    inp2send = inp + inp1
+
+print(inp2send)
+packedObj = pack("iii", inp, inp1, inp2)
+
+print(packedObj)
+s.sendto(packedObj,serverAddrPort)
+
+msg = s.recvfrom(bufferSize)
+unpackedObj = unpack("ii", msg)
+
+print(unpackedObj)
