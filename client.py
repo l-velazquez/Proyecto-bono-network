7b414a94d4bc0bbd99709ddf9da9846c5a90583a
l-velazquez <luisfernandojavier.velazquez@upr.edu>
2022-11-09 19:15:54 -0400

    Added bug fixes

--- client.py
+++ client.py
@@ -13,6 +13,9 @@ s.connect(serverAddrPort)
 s.settimeout(1)
 
 #s.bind(serverAddrPort)
+msg = "Hola"
+s.send(msg.encode())
+recvMsg = s.recv(bufferSize)
 
 #Print to terminal for the client
 print("\n\nPlease choose one operation:\n\t1.Sum\n\t2.Substraction\n\t3.Muliplication\n\t4.Division\n\t5.Factorial\n\t6.Summatory")
@@ -20,28 +23,22 @@ inp = int(input("\nOperation >>> "))
 
 if(inp == (1 or 2 or 3 or 4)):
     print("Please add two inputs:")
-    inp1 = int(input("Input 1 >>>"))
-    inp2 = int(input("Input 2 >>>"))
-    inp2send = (inp,inp1,inp2)
+    inp1 = pack("i",int(input("Input 1 >>>")))
+    inp2 = pack("i",int(input("Input 2 >>>")))
+    inp0 = pack("i", inp)
+    inp2send = (inp0+inp1+inp2)
 else:
     print("Input number")
-    inp1 = int(input("Input >>>"))
+    inp1 = pack("i", int(input("Input >>>")))
+    inp0 = pack("i",inp)
     inp2send = inp + inp1
 
 print(inp2send)
-packedObj1 = pack("i", inp)
-packedObj2 = pack("i", inp1)
-packedObj3 = pack("i", inp2)
 
-print("sending:",packedObj1)
-s.send(packedObj1)
+s.send(inp2send)
 #s.send(packedObj2)
 #s.send(packedObj3)
 #unpackedObj = unpack("iii",packedObj)
 #print(unpackedObj)
 
-#1
-msg = s.recv(bufferSize)
-print(msg)
-msg1 = msg.decode()
-print(msg1)
+unpack("i",data[1:5])
